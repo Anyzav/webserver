60c4cbfecb4fa717164bafbe79306b74b5b18b0f
User <dasha-gen08@mail.ru>
2024-04-09 19:32:08 +0300

    /

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import telebot
 
 bot = telebot.TeleBot('7032417900:AAFnXx--IFE8Nb71hiefFj4HCimo5QwuSVo')
 
+@bot.message_handler(commands=['start',])
+def main(message):
+    bot.send_message(message.chat.id, 'Привет!')
+
+bot.infinity_polling()
